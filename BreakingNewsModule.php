6f7956ad0f3918046b7d0a980afd4f6c435c7684
Lucas Bartholemy <lucas@bartholemy.com>
2014-06-28 14:22:49 +0200

    Module API Changes

--- BreakingNewsModule.php
+++ BreakingNewsModule.php
@@ -1,20 +1,24 @@
 <?php
 
-class BreakingNewsModule extends CWebModule {
+class BreakingNewsModule extends HWebModule
+{
 
     /**
      * On Init of Dashboard Sidebar, add the widget
      * 
      * @param type $event
      */
-    public static function onDashboardSidebarInit($event) {
-        if (Yii::app()->moduleManager->isEnabled('breakingnews')) {
-            
-            if (HSetting::Get('active', 'breakingnews') && UserSetting::Get(Yii::app()->user->id, 'seen', 'breakingnews') != 1) {
-                UserSetting::Set(Yii::app()->user->id, 'seen', true, 'breakingnews');
-                $event->sender->addWidget('application.modules.breakingnews.widgets.BreakingNewsWidget', array(), array('sortOrder' => 1));
-            }
+    public static function onDashboardSidebarInit($event)
+    {
+        if (HSetting::Get('active', 'breakingnews') && UserSetting::Get(Yii::app()->user->id, 'seen', 'breakingnews') != 1) {
+            UserSetting::Set(Yii::app()->user->id, 'seen', true, 'breakingnews');
+            $event->sender->addWidget('application.modules.breakingnews.widgets.BreakingNewsWidget', array(), array('sortOrder' => 1));
         }
+    }
+
+    public function getConfigUrl()
+    {
+        return Yii::app()->createUrl('//breakingnews/admin/index');
     }
 
 }
--- autostart.php
+++ autostart.php
@@ -3,13 +3,10 @@
 Yii::app()->moduleManager->register(array(
     'id' => 'breakingnews',
     'class' => 'application.modules.breakingnews.BreakingNewsModule',
-    'title' => Yii::t('BreakingNewsModule.base', 'BreakingNews'),
-    'description' => Yii::t('ExampleModule.base', 'Shows a breaking news popup on dashboard.'),
-    'configRoute' => '//breakingnews/admin',
     'import' => array(
         'application.modules.breakingnews.*',
     ),
-    // Events to Catch 
+    // Events to Catch
     'events' => array(
         array('class' => 'DashboardSidebarWidget', 'event' => 'onInit', 'callback' => array('BreakingNewsModule', 'onDashboardSidebarInit')),
     ),
