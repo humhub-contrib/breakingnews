451e0de4836793eb5ade16e5af9d0144f60ebdff
Lucas Bartholemy <lucas@bartholemy.com>
2014-06-28 13:48:38 +0200

    Module API Changes

--- BreakingNewsModule.php
+++ BreakingNewsModule.php
@@ -1,24 +1,19 @@
 <?php
 
-class BreakingNewsModule extends HWebModule
-{
-
-    public function getConfigUrl()
-    {
-        return Yii::app()->createUrl('//breakingnews/admin');
-    }
+class BreakingNewsModule extends CWebModule {
 
     /**
      * On Init of Dashboard Sidebar, add the widget
      * 
      * @param type $event
      */
-    public static function onDashboardSidebarInit($event)
-    {
-
-        if (HSetting::Get('active', 'breakingnews') && UserSetting::Get(Yii::app()->user->id, 'seen', 'breakingnews') != 1) {
-            UserSetting::Set(Yii::app()->user->id, 'seen', true, 'breakingnews');
-            $event->sender->addWidget('application.modules.breakingnews.widgets.BreakingNewsWidget', array(), array('sortOrder' => 1));
+    public static function onDashboardSidebarInit($event) {
+        if (Yii::app()->moduleManager->isEnabled('breakingnews')) {
+            
+            if (HSetting::Get('active', 'breakingnews') && UserSetting::Get(Yii::app()->user->id, 'seen', 'breakingnews') != 1) {
+                UserSetting::Set(Yii::app()->user->id, 'seen', true, 'breakingnews');
+                $event->sender->addWidget('application.modules.breakingnews.widgets.BreakingNewsWidget', array(), array('sortOrder' => 1));
+            }
         }
     }
 
--- autostart.php
+++ autostart.php
@@ -3,6 +3,9 @@
 Yii::app()->moduleManager->register(array(
     'id' => 'breakingnews',
     'class' => 'application.modules.breakingnews.BreakingNewsModule',
+    'title' => Yii::t('BreakingNewsModule.base', 'BreakingNews'),
+    'description' => Yii::t('ExampleModule.base', 'Shows a breaking news popup on dashboard.'),
+    'configRoute' => '//breakingnews/admin',
     'import' => array(
         'application.modules.breakingnews.*',
     ),
