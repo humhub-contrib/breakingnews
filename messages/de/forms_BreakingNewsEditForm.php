4392bf02d3e2c990de9c0ab0a1ba594969ef8276
Lucas Bartholemy <lucas@bartholemy.com>
2014-10-03 06:03:57 +0200

    Updated translations (translate.humhub.org)

--- messages/de/forms_BreakingNewsEditForm.php
+++ messages/de/forms_BreakingNewsEditForm.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'Active' => 'Aktiv',
-  'Mark as unseen for all users' => 'Für alle als ungelesen markieren',
+  'Mark as unseen for all users' => 'Für alle Nutzer als ungelesen markieren',
   'Message' => 'Nachricht',
   'Title' => 'Titel',
 );
--- messages/de/views_admin_index.php
+++ messages/de/views_admin_index.php
@@ -2,7 +2,7 @@
 return array (
   'Back to modules' => 'zurück zu den Modulen',
   'Breaking News Configuration' => 'Konfiguration Eilmeldungen',
-  'Note: You can use markdown syntax.' => 'Hinweis: Sie können die Markdown Sytax verwenden.
+  'Note: You can use markdown syntax.' => 'Hinweis: Du kannst die Markdown Sytax verwenden.
 (Weitere Infos unter http://www.markdown.de)',
   'Save' => 'Speichern',
 );
