1177afc5d55a28388dd19a5314c437658d6e3109
Lucas Bartholemy <lucas@bartholemy.com>
2015-01-23 12:40:09 +0100

    Updated translations (translate.humhub.org)

--- messages/pt_br/views_admin_index.php
+++ messages/pt_br/views_admin_index.php
@@ -1,7 +1,7 @@
 <?php
 return array (
-  'Back to modules' => '',
-  'Breaking News Configuration' => '',
-  'Note: You can use markdown syntax.' => '',
+  'Back to modules' => 'Voltar para módulos',
+  'Breaking News Configuration' => 'Configuração Breaking News',
+  'Note: You can use markdown syntax.' => 'Nota: Você pode usar a sintaxe markdown.',
   'Save' => 'Salvar',
 );
