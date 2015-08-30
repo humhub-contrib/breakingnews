86573ca1d4206752bf2831ce34fe960b132b273f
andystrobel <andreas.strobel@googlemail.com>
2015-08-30 18:22:59 +0200

    Fixed modal window

--- views/index/index.php
+++ views/index/index.php
@@ -1,36 +1,17 @@
-<?php ?>
-
-<div class="modal-dialog">
-    <div class="modal-content">
-        <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h4 class="modal-title" id="myModalLabel"><?php echo $title; ?></h4>
-        </div>
-        <div class="modal-body">
-<?php
-echo \yii\helpers\Markdown::process($message);
-?>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-primary"
-                    data-dismiss="modal"><?php echo Yii::t('BreakingnewsModule.views_index', 'Close'); ?></button>
-        </div>
-
-    </div>
+<div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+    <h4 class="modal-title" id="myModalLabel"><?php echo $title; ?></h4>
+</div>
+<div class="modal-body">
+    <?php
+    echo \yii\helpers\Markdown::process($message);
+    ?>
+</div>
+<div class="modal-footer">
+    <button type="button" class="btn btn-primary"
+            data-dismiss="modal"><?php echo Yii::t('BreakingnewsModule.views_index', 'Close'); ?></button>
 </div>
 
 
-<script type="text/javascript">
 
-    /*
-     * Modal handling by close event
-     */
-    $('#globalModal').on('hidden.bs.modal', function (e) {
 
-        // Reload whole page (to see changes on it)
-        //window.location.reload();
-
-        // just close modal and reset modal content to default (shows the loader)
-        $('#globalModal').html('<div class="modal-dialog"><div class="modal-content"><div class="modal-body"><div class="loader"></div></div></div></div>');
-    })
-</script>
