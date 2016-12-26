d7fb05064fd2f9df07d6a875588849c544fbf7e6
Ralph <RHITNL@users.noreply.github.com>
2016-12-26 17:16:01 +0100

    Updated NL Translation
    Translated only forgotten word in file to Dutch

--- messages/nl/views_admin_index.php
+++ messages/nl/views_admin_index.php
@@ -3,5 +3,5 @@ return array (
   'Back to modules' => 'Terug naar modules',
   'Breaking News Configuration' => 'Breaking News Configuratie',
   'Note: You can use markdown syntax.' => 'Let op: Je kunt markdown syntax gebruiken',
-  'Save' => 'Enregistrer',
+  'Save' => 'Opslaan',
 );
