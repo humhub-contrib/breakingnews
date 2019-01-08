31f857a54224ccf7aa6ee264754ae96bf19d13f3
HumHub Translations <info@humhub.com>
2019-01-08 11:46:12 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/views_admin_index.php
+++ messages/hu/views_admin_index.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'Back to modules' => 'Vissza a modulokhoz',
-  'Breaking News Configuration' => 'Fontos Hírek Beállítása',
+  'Breaking News Configuration' => 'Fontos hírek beállítása',
   'Note: You can use markdown syntax.' => 'Megjegyzés: Használhatsz "markdown" szintaxisokat.',
   'Save' => 'Mentés',
 );
