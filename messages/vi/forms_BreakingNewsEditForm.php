985bc03b9520415411ee6616c7239d4da6d7a95f
HumHub Translations <info@humhub.com>
2020-03-17 09:17:07 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/vi/forms_BreakingNewsEditForm.php
+++ messages/vi/forms_BreakingNewsEditForm.php
@@ -1,7 +1,7 @@
 <?php
 return array (
-  'Active' => '',
-  'Mark as unseen for all users' => '',
+  'Active' => 'Hoạt động',
+  'Mark as unseen for all users' => 'Đánh dấu \'chưa xem\' cho tất cả người dùng',
   'Message' => 'Tin nhắn',
   'Title' => 'Tiêu đề',
 );
