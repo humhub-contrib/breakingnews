6c2041271514b761bbbd18b098c6fb7a53ddba10
HumHub Translations <info@humhub.com>
2021-02-08 13:53:13 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt/views_admin_index.php
+++ messages/pt/views_admin_index.php
@@ -2,6 +2,6 @@
 return array (
   'Back to modules' => 'Voltar para os módulos',
   'Breaking News Configuration' => 'Configurações de notícias de última hora',
-  'Note: You can use markdown syntax.' => 'Nota: Você pode utilizar a sintaxe markdown.',
+  'Note: You can use markdown syntax.' => 'Nota: Podes utilizar a sintaxe markdown.',
   'Save' => 'Guardar',
 );
