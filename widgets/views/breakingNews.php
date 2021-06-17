8e450f4024728f16c06a1d5e8f9d4f7918e3c4f9
yurabakhtin <yurybakh@gmail.com>
2021-06-17 14:44:33 +0300

    Use nonce protection for javascript

--- widgets/views/breakingNews.php
+++ widgets/views/breakingNews.php
@@ -1,8 +1,9 @@
 <?php
 
+use humhub\libs\Html;
 use yii\helpers\Url;
 ?>
-<script type="text/javascript">
+<script <?= Html::nonce() ?>>
     $(document).ready(function () {
         $('#globalModal').modal('show');
         $('#globalModal .modal-content').load('<?php echo Url::to(['/breakingnews/index']); ?>');
