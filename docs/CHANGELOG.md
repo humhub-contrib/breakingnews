aa04270a5feff4065bdb9b4e766a7b2259cab01d
yurabakhtin <yurybakh@gmail.com>
2021-06-17 10:48:11 +0300

    Fix content loading on PJAX request

---
+++ docs/CHANGELOG.md
@@ -0,0 +1,6 @@
+Changelog
+=========
+
+1.0.4 (Unreleased)
+---------------------
+- Fix #10: Fix content loading on PJAX request
--- widgets/views/breakingNews.php
+++ widgets/views/breakingNews.php
@@ -3,14 +3,8 @@
 use yii\helpers\Url;
 ?>
 <script type="text/javascript">
-
     $(document).ready(function () {
-
-        $('#globalModal').modal({
-            remote: '<?php echo Url::to(['/breakingnews/index']); ?>',
-            show: true
-        })
-
+        $('#globalModal').modal('show');
+        $('#globalModal .modal-content').load('<?php echo Url::to(['/breakingnews/index']); ?>');
     });
-
 </script>
