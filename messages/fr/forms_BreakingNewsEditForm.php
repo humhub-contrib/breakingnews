fc55a7afd49724d47c565cc58361d46913ae2459
HumHub Translations <hello@humhub.com>
2022-11-16 11:47:26 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/fr/forms_BreakingNewsEditForm.php
+++ messages/fr/forms_BreakingNewsEditForm.php
@@ -1,10 +1,9 @@
 <?php
-
-return [
-    'Active' => 'Actif',
-    'Mark as unseen for all users' => 'Marquer comme "non lu" pour tous les utilisateurs',
-    'Message' => 'Message',
-    'Title' => 'Titre',
-    'Groups whose members will see this breaking news' => '',
-    'If no group is ticked, everyone will see this breaking news.' => '',
-];
+return array (
+  'Active' => 'Actif',
+  'Groups whose members will see this breaking news' => 'Groupes dont les membres verront cette nouvelle',
+  'If no group is ticked, everyone will see this breaking news.' => 'Si aucun groupe n’est coché, tout le monde verra cette nouvelle.',
+  'Mark as unseen for all users' => 'Marquer comme "non lu" pour tous les utilisateurs',
+  'Message' => 'Message',
+  'Title' => 'Titre',
+);
--- messages/fr/views_admin_index.php
+++ messages/fr/views_admin_index.php
@@ -1,8 +1,7 @@
 <?php
-
-return [
-    'Back to modules' => 'Retour aux modules',
-    'Breaking News Configuration' => 'Configuration du module Breaking News',
-    'Note: You can use markdown syntax.' => 'Note : vous pouvez utiliser la syntaxe Markdown',
-    'Groups restriction' => '',
-];
+return array (
+  'Back to modules' => 'Retour aux modules',
+  'Breaking News Configuration' => 'Configuration du module Breaking News',
+  'Groups restriction' => 'Restriction de groupes',
+  'Note: You can use markdown syntax.' => 'Note : vous pouvez utiliser la syntaxe Markdown',
+);
