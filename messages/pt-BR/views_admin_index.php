7ee8872d5d98972603c60862802233968e4dea32
HumHub Translations <hello@humhub.com>
2022-11-25 17:36:28 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt-BR/views_admin_index.php
+++ messages/pt-BR/views_admin_index.php
@@ -1,8 +1,7 @@
 <?php
-
-return [
-    'Back to modules' => 'Voltar para módulos',
-    'Breaking News Configuration' => 'Configuração Breaking News',
-    'Note: You can use markdown syntax.' => 'Nota: Você pode usar a sintaxe markdown.',
-    'Groups restriction' => '',
-];
+return array (
+  'Back to modules' => 'Voltar para módulos',
+  'Breaking News Configuration' => 'Configuração das Últimas Notícias',
+  'Groups restriction' => 'Restrição de grupos',
+  'Note: You can use markdown syntax.' => 'Nota: Você pode usar a sintaxe markdown.',
+);
