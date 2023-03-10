c34623f1a3de96634b8e497f386cd9eab1d3023f
HumHub Translations <hello@humhub.com>
2023-03-10 14:54:37 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/forms_BreakingNewsEditForm.php
+++ messages/de/forms_BreakingNewsEditForm.php
@@ -1,10 +1,9 @@
 <?php
-
-return [
-    'Active' => 'Aktiv',
-    'Mark as unseen for all users' => 'Für alle Nutzer als ungelesen markieren',
-    'Message' => 'Nachricht',
-    'Title' => 'Titel',
-    'Groups whose members will see this breaking news' => '',
-    'If no group is ticked, everyone will see this breaking news.' => '',
-];
+return array (
+  'Active' => 'Aktiv',
+  'Groups whose members will see this breaking news' => 'Gruppen, deren Mitglieder diese Meldung sehen werden',
+  'If no group is ticked, everyone will see this breaking news.' => 'Wenn keine Gruppe markiert ist, sieht jeder Nutzer diese Meldung.',
+  'Mark as unseen for all users' => 'Für alle Nutzer als ungelesen markieren',
+  'Message' => 'Nachricht',
+  'Title' => 'Titel',
+);
