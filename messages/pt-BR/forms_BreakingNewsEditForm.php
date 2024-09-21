c1d8056562a669a06b755a3d22e979a058ab8784
HumHub Translations <hello@humhub.com>
2024-09-21 08:24:01 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt-BR/forms_BreakingNewsEditForm.php
+++ messages/pt-BR/forms_BreakingNewsEditForm.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    'Active' => 'Ativo',
-    'Groups whose members will see this breaking news' => 'Grupos cujos membros verão estas Últimas Notícias',
-    'If no group is ticked, everyone will see this breaking news.' => 'Se nenhum grupo estiver marcado, todos verão estas Últimas Notícias.',
-    'Mark as unseen for all users' => 'Marcar como não-visto para todos usuários',
-    'Message' => 'Mensagem',
-    'Title' => 'Título',
-    'Expired at' => '',
-];
+return array (
+  'Active' => 'Ativo',
+  'Expired at' => 'Expirado em',
+  'Groups whose members will see this breaking news' => 'Grupos cujos membros verão estas Últimas Notícias',
+  'If no group is ticked, everyone will see this breaking news.' => 'Se nenhum grupo estiver marcado, todos verão estas Últimas Notícias.',
+  'Mark as unseen for all users' => 'Marcar como não-visto para todos usuários',
+  'Message' => 'Mensagem',
+  'Title' => 'Título',
+);
