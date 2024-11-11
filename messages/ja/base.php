582d0f9800ee676a8b9172b51da771049985b5d8
HumHub Translations <hello@humhub.com>
2024-11-11 11:33:10 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/base.php
+++ messages/ja/base.php
@@ -1,16 +1,15 @@
 <?php
-
 return [
-    'Active' => 'アクティブ',
-    'Back to modules' => 'モジュールへ戻る',
-    'Breaking News Configuration' => 'ニュース速報の設定',
-    'Close' => '閉じる',
-    'Groups restriction' => 'グループの制限',
-    'Groups whose members will see this breaking news' => 'このニュース速報がメンバーに表示されるグループ',
-    'If no group is ticked, everyone will see this breaking news.' => 'どのグループもチェックされていない場合、全員がこのニュース速報を見ることができます。',
-    'Mark as unseen for all users' => 'すべてのユーザーに見えない状態としてマーク',
-    'Message' => 'メッセージ',
-    'Note: You can use markdown syntax.' => '注：マークダウン構文を使用することができます。',
-    'Title' => 'タイトル',
-    'Expire' => '',
+  'Active' => 'アクティブ',
+  'Back to modules' => 'モジュールへ戻る',
+  'Breaking News Configuration' => 'ニュース速報の設定',
+  'Close' => '閉じる',
+  'Expire' => '期限切れ',
+  'Groups restriction' => 'グループの制限',
+  'Groups whose members will see this breaking news' => 'このニュース速報がメンバーに表示されるグループ',
+  'If no group is ticked, everyone will see this breaking news.' => 'どのグループもチェックされていない場合、全員がこのニュース速報を見ることができます。',
+  'Mark as unseen for all users' => 'すべてのユーザーに見えない状態としてマーク',
+  'Message' => 'メッセージ',
+  'Note: You can use markdown syntax.' => '注：マークダウン構文を使用することができます。',
+  'Title' => 'タイトル',
 ];
