775f4f35b01fad3ac5bd89e7598f46d4c7a6bdf6
HumHub Translations <hello@humhub.com>
2024-12-16 09:08:34 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt-BR/base.php
+++ messages/pt-BR/base.php
@@ -1,16 +1,15 @@
 <?php
-
 return [
-    'Active' => 'Ativo',
-    'Back to modules' => 'Voltar para módulos',
-    'Breaking News Configuration' => 'Configuração das Últimas Notícias',
-    'Close' => 'Fechar',
-    'Groups restriction' => 'Restrição de grupos',
-    'Groups whose members will see this breaking news' => 'Grupos cujos membros verão estas Últimas Notícias',
-    'If no group is ticked, everyone will see this breaking news.' => 'Se nenhum grupo estiver marcado, todos verão estas Últimas Notícias.',
-    'Mark as unseen for all users' => 'Marcar como não-visto para todos usuários',
-    'Message' => 'Mensagem',
-    'Note: You can use markdown syntax.' => 'Nota: Você pode usar a sintaxe markdown.',
-    'Title' => 'Título',
-    'Expire' => '',
+  'Active' => 'Ativo',
+  'Back to modules' => 'Voltar para módulos',
+  'Breaking News Configuration' => 'Configuração das Últimas Notícias',
+  'Close' => 'Fechar',
+  'Expire' => 'Expirar',
+  'Groups restriction' => 'Restrição de grupos',
+  'Groups whose members will see this breaking news' => 'Grupos cujos membros verão estas Últimas Notícias',
+  'If no group is ticked, everyone will see this breaking news.' => 'Se nenhum grupo estiver marcado, todos verão estas Últimas Notícias.',
+  'Mark as unseen for all users' => 'Marcar como não-visto para todos usuários',
+  'Message' => 'Mensagem',
+  'Note: You can use markdown syntax.' => 'Nota: Você pode usar a sintaxe markdown.',
+  'Title' => 'Título',
 ];
