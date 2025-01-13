a277bc053a30de498c77853a34b165ca844f754d
HumHub Translations <hello@humhub.com>
2025-01-13 10:50:51 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/sv/base.php
+++ messages/sv/base.php
@@ -1,16 +1,15 @@
 <?php
-
 return [
-    'Active' => 'Aktiv',
-    'Back to modules' => 'Tillbaka till moduler',
-    'Breaking News Configuration' => 'Inställningar för Senaste nytt',
-    'Close' => 'Stäng',
-    'Groups restriction' => 'Gruppbegränsning',
-    'Groups whose members will see this breaking news' => 'Grupper vars medlemmar kommer att se de här nyheterna',
-    'If no group is ticked, everyone will see this breaking news.' => 'Om ingen grupp är ikryssad kommer alla att se de här nyheterna.',
-    'Mark as unseen for all users' => 'Markera som inte läst för alla användare',
-    'Message' => 'Meddelande',
-    'Note: You can use markdown syntax.' => 'Obs: du kan använda markdown syntax',
-    'Title' => 'Rubrik',
-    'Expire' => '',
+  'Active' => 'Aktiv',
+  'Back to modules' => 'Tillbaka till moduler',
+  'Breaking News Configuration' => 'Inställningar för Senaste nytt',
+  'Close' => 'Stäng',
+  'Expire' => 'Upphör',
+  'Groups restriction' => 'Gruppbegränsning',
+  'Groups whose members will see this breaking news' => 'Grupper vars medlemmar kommer att se de här nyheterna',
+  'If no group is ticked, everyone will see this breaking news.' => 'Om ingen grupp är ikryssad kommer alla att se de här nyheterna.',
+  'Mark as unseen for all users' => 'Markera som inte läst för alla användare',
+  'Message' => 'Meddelande',
+  'Note: You can use markdown syntax.' => 'Obs: du kan använda markdown syntax',
+  'Title' => 'Rubrik',
 ];
