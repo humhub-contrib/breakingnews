98bad90f61da4e11641116482f753280de98ad11
Marc Farré <contact@marc.fun>
2025-06-11 13:24:27 +0100

    Fix modal - https://github.com/humhub-contrib/breakingnews/pull/42#issuecomment-2956953193

--- views/index/index.php
+++ views/index/index.php
@@ -1,16 +1,16 @@
 <?php
 
+use humhub\modules\content\widgets\richtext\converter\RichTextToHtmlConverter;
 use humhub\widgets\modal\Modal;
 use humhub\widgets\modal\ModalButton;
-use humhub\modules\content\widgets\richtext\converter\RichTextToHtmlConverter;
 
 /* @var string $title */
 /* @var string $message */
 ?>
-<?= Modal::beginDialog([
+<?php Modal::beginDialog([
     'title' => $title,
     'footer' => ModalButton::cancel(Yii::t('BreakingnewsModule.base', 'Close')),
     'dialogOptions' => ['data-ui-markdown' => true],
 ]) ?>
     <?= RichTextToHtmlConverter::process($message) ?>
-<?= Modal::endDialog() ?>
+<?php Modal::endDialog() ?>
--- widgets/views/breakingNews.php
+++ widgets/views/breakingNews.php
@@ -2,10 +2,12 @@
 
 use humhub\helpers\Html;
 use yii\helpers\Url;
+
 ?>
 <script <?= Html::nonce() ?>>
     $(document).ready(function () {
-        var modal = humhub.require('humhub.modules.ui.modal');
-        modal.global.load('<?php echo Url::to(['/breakingnews/index']); ?>');
+        setTimeout(function () {
+            humhub.modules.ui.modal.global.load("<?= Url::to(['/breakingnews/index']) ?>");
+        }, 500);
     });
 </script>
