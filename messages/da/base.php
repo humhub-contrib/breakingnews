df52a802b1e8b998ac15b7127553e084715c74bb
HumHub Translations <hello@humhub.com>
2025-09-10 17:07:06 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/da/base.php
+++ messages/da/base.php
@@ -1,16 +1,15 @@
 <?php
-
 return [
-    'Active' => 'Aktiv',
-    'Back to modules' => 'Tilbage til moduler',
-    'Breaking News Configuration' => 'Breaking News Konfiguration',
-    'Close' => 'Luk',
-    'Mark as unseen for all users' => 'Markér som uset for alle brugere',
-    'Message' => 'Besked',
-    'Note: You can use markdown syntax.' => 'Bemærk: Du kan bruge markdown syntaks.',
-    'Title' => 'Titel',
-    'Expire' => '',
-    'Groups restriction' => '',
-    'Groups whose members will see this breaking news' => '',
-    'If no group is ticked, everyone will see this breaking news.' => '',
+  'Active' => 'Aktiv',
+  'Back to modules' => 'Tilbage til moduler',
+  'Breaking News Configuration' => 'Konfiguration af breaking news',
+  'Close' => 'Luk',
+  'Expire' => 'Udløber',
+  'Groups restriction' => 'Begrænsning af grupper',
+  'Groups whose members will see this breaking news' => 'Grupper, hvis medlemmer vil se denne nyhed',
+  'If no group is ticked, everyone will see this breaking news.' => 'Hvis ingen gruppe er markeret, vil alle se denne nyhed.',
+  'Mark as unseen for all users' => 'Marker som uset for alle brugere',
+  'Message' => 'Besked',
+  'Note: You can use markdown syntax.' => 'Bemærk: Du kan bruge markdown-syntaks.',
+  'Title' => 'Titel',
 ];
